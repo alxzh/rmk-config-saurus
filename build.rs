c63834f1ae5149b093327b5ac3f4954d3dca12ea
Alex Zhavnerchik <alex.vizor@gmail.com>
2026-08-30 19:01:40 -0700

    Trying out rynk

--- build.rs
+++ build.rs
@@ -1,25 +1,18 @@
 //! Build script for Saurus keyboard firmware.
 //!
-//! Copies memory.x linker script, compresses vial.json for Vial support,
-//! and sets linker flags.
+//! Prepares the macro configuration, copies the memory.x linker script, and
+//! sets linker flags.
 
 use std::fs::File;
-use std::io::{Read, Write};
+use std::io::Write;
 use std::path::{Path, PathBuf};
 use std::{env, fs};
-
-use const_gen::*;
-use xz2::read::XzEncoder;
 
 fn main() {
     println!("cargo:rerun-if-changed=keyboard.toml");
 
     let out = PathBuf::from(env::var_os("OUT_DIR").unwrap());
     generate_macro_keyboard_config(&out);
-
-    // Generate vial config at the root of project
-    println!("cargo:rerun-if-changed=vial.json");
-    generate_vial_config();
 
     // Put `memory.x` in our output directory and ensure it's
     // on the linker search path.
@@ -70,33 +63,4 @@ fn generate_macro_keyboard_config(out: &Path) {
     let path = out.join("keyboard-macro.toml");
     fs::write(&path, macro_config).expect("Cannot write macro keyboard config");
     println!("cargo:rustc-env=KEYBOARD_TOML_PATH={}", path.display());
-}
-
-fn generate_vial_config() {
-    let out_file = Path::new(&env::var_os("OUT_DIR").unwrap()).join("config_generated.rs");
-
-    let p = Path::new("vial.json");
-    let mut content = String::new();
-    match File::open(p) {
-        Ok(mut file) => {
-            file.read_to_string(&mut content)
-                .expect("Cannot read vial.json");
-        }
-        Err(e) => println!("Cannot find vial.json {:?}: {}", p, e),
-    };
-
-    let vial_cfg = json::stringify(json::parse(&content).unwrap());
-    let mut keyboard_def_compressed: Vec<u8> = Vec::new();
-    XzEncoder::new(vial_cfg.as_bytes(), 6)
-        .read_to_end(&mut keyboard_def_compressed)
-        .unwrap();
-
-    let keyboard_id: Vec<u8> = vec![0xD8, 0x6D, 0xBE, 0x47, 0x8A, 0x0E, 0x21, 0xC5];
-    let const_declarations = [
-        const_declaration!(pub VIAL_KEYBOARD_DEF = keyboard_def_compressed),
-        const_declaration!(pub VIAL_KEYBOARD_ID = keyboard_id),
-    ]
-    .map(|s| "#[allow(clippy::redundant_static_lifetimes)]\n".to_owned() + s.as_str())
-    .join("\n");
-    fs::write(out_file, const_declarations).unwrap();
 }
